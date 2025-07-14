749ad1567feeab9c7882f956c05a0de5e3bf6db5
IGraceH <grace10312@outlook.com>
2025-07-14 14:29:29 -0700

    Added buttons

---
+++ src/Directory.jsx
@@ -0,0 +1,17 @@
+
+function Button() {
+    return (
+      <button>Text</button>
+    )
+  }
+
+function Directory() {  
+    return (
+      <div>
+        <Button></Button>
+        <Button></Button>
+      </div>
+    )
+  }
+  
+  export default Directory
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Directory from './Directory.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Directory />
   </StrictMode>,
 )
