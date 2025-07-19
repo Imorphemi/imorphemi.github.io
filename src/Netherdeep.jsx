a01f48c3e18ef698c53f4b52dbbb9555d14c214b
IGraceH <grace10312@outlook.com>
2025-07-18 21:04:17 -0700

    Image Gallery

--- src/Netherdeep.jsx
+++ src/Netherdeep.jsx
@@ -1,19 +1,35 @@
 import { createRoot } from 'react-dom/client'
+import { imageList } from './images.js'
+import './Netherdeep.css'
 
+
+const gallery = imageList.map(sec =>
+  <div key={sec.section}>
+    <h1>{sec.name}</h1>
+    <GallerySection folder={sec.section} images={sec.images} />
+  </div>
+);
 
 function Image({url}) {
-    return (
-      <img src={url} width="500"></img>
-    )
-  }
+  return (
+    <img className="gallery-img" src={url}></img>
+  )
+}
+
+function GallerySection({folder, images}) {
+  return (
+    images.map(image => <Image key={image.filename} url={'./netherdeep/img/'+ folder+ '/' + image.filename} />)
+  )
+}
+
 
 function Netherdeep() {  
-    return (
-      <div>
-        {}
-      </div>
-    )
-  }
+  return (
+    <div>
+      {gallery}
+    </div>
+  )
+}
   
   createRoot(document.getElementById('root')).render(
     <Netherdeep />,
